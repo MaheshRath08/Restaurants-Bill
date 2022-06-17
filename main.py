be8890fad07236818d3b7789936e14225c44eeb1
MaheshRath08 <maheshrath.2002@gmail.com>
2022-06-17 10:41:56 +0530

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print("Thanks for coming in...")
+print("Hopefully You had an amazing experience...")
 subTotal = float(input("Sub-Total Amount:\n"))
 tip = float(input("Tips respect to the sub-total Amount (in %):\n"))
 gst = 0.05
